d8adfece2a61cd3f441192e50344b989a7c61b07
Colby Aley <colby@aley.me>
2019-02-26 13:14:07 -0800

    Replace express with Now's thing

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-const express = require('express');
-
-const app = express();
-
-app.get('/', function(req, res) {
-  res.send({ foo: 'bar' });
-});
-
-app.listen(3000, function() {
-  console.log('listening on port 3000');
-});
+module.exports = (req, res) => {
+  res.end({ works: 'true' });
+};
